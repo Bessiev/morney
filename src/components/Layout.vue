7be1ef8a19163cd580fdafbe007515b71500e711
Bessie <1976177625@qq.com>
2021-07-05 07:33:03 +0800

    __WebpackModuleApi未定义

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -8,7 +8,6 @@
 </template>
 
 <script lang="ts">
-
 export default {
     name:'Layout',
 }
@@ -16,13 +15,11 @@ export default {
 
 <style lang="scss" scoped>
     .nav-wrapper{
-        border:1px solid green;
         display: flex;
         flex-direction: column;
         height: 100vh;//固定高度
     }
     .content{
-        border:1px solid blue;
         flex-grow: 1;//尽量把所有高度给content
         overflow:auto;//内容多的时候可滚动
     }
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,10 @@
-/*eslint-disable */
+/* eslint-disable */ 
 const path = require('path')
 
 module.exports = {
   lintOnSave: false,
   chainWebpack: config =>{
     const dir = path.resolve(__dirname, 'src/assets/icons')
-
     config.module
       .rule('svg-sprite')
       .test(/\.svg$/)//正则匹配
