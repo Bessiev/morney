89f6ae226fce48ed9d63f99e675ff442ace19b08
Bessie <1976177625@qq.com>
2021-07-05 04:48:09 +0800

    重复三次布局

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
+    <div class="nav-wrapper">
+        <div class="content">
         Statistics.vue
+        </div>
         <Nav/>
     </div>
 </template>
@@ -14,5 +16,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+ .nav-wrapper{
+        border:1px solid green;
+        display: flex;
+        flex-direction: column;
+        height: 100vh;//固定高度
+    }
+    .content{
+        border:1px solid blue;
+        flex-grow: 1;//尽量把所有高度给content
+        overflow:auto;//内容多的时候可滚动
+    }
 </style>
